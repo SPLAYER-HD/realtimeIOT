be0fdef2da4b4e7ef30c0a5860cd9045b7732888
Diego <diego.torres@fitideas.co>
2020-07-18 16:15:28 +1000

    add react to front module

---
+++ app/realtime-front/App.js
@@ -0,0 +1,10 @@
+'use strict'
+import React from 'react'
+import './App.css'
+
+const App = () => (
+    <div className="App">
+        <h1>Hello realtime</h1>
+    </div>
+)
+export default App
---
+++ app/realtime-front/index.js
@@ -0,0 +1,10 @@
+'use strict'
+import React from 'react'
+import ReactDom from 'react-dom'
+import App from '.App.js'
+
+ReactDom.render(
+    <App/>,
+    document.getElementById('root')
+)
+export default App
